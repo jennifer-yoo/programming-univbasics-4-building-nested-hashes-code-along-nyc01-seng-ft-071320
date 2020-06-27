a959f5c09dc6b177a6ec2c769263aed1c54630ab
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-27 03:21:26 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+        :patriarch => { name: "Lord Montague", Age: "53"}, 
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+      },
+     :capulet => {
+       :patriarch => {},
+       :matriach => {},
+       :heroine => {},
+       :heroine_friends => {}
+     }
+   }
 end
